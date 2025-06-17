caae4fc935e5410ac6e7d6c566efe3e3f4be8e4d
Nucl3arSn3k <shredderofwheat@gmail.com>
2025-06-17 18:17:43 -0400

    Basic UI start
    Have started laying down the bones of the UI

--- src/emustatus.rs
+++ src/emustatus.rs
@@ -1,4 +1,4 @@
-struct Chip8Emu{
+pub struct Chip8Emu{
     memory: [u8; 4096], //Memory for chip8
     gpr: [u8;16],//gp registers
     ir:u16, //index register
@@ -6,15 +6,37 @@ struct Chip8Emu{
     sp:u8,//stack pointer
     dt:u8,//Delay timer
     st:u8,//Sound timer
-
+    stack:Vec<u16>,//stack
+    display: [[u8; 32]; 64],//displayvalues
 }
 
 impl Chip8Emu{ //Functions for emulator
-    pub fn memorymap(){
+
+
+    pub fn new() -> Self{
+        Chip8Emu{
+            memory: [0; 4096],
+            gpr: [0;16],
+            ir: 0,
+            pc: 0x200,
+            sp: 0,
+            dt: 0,
+            st: 0,
+            stack: Vec::new(),
+            display: [[0;32];64],
+        }
+
+
+    }
+
+    pub fn memorymap(&self){ //Hardcoded for now
 
 
 
 
     }
+
+
+    pub fn displaytest(){}
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-mod emustatus;
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![allow(rustdoc::missing_crate_level_docs)]
 
-fn main() {
-    println!("Hello, world!");
+mod emustatus;
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    env_logger::init();
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+
+    let  mut emulator = emustatus::Chip8Emu::new(); //init
+
+    eframe::run_simple_native("Chip8Emu", options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            //ui.heading("CHIP-8 Emulator");
+
+            //Emu UI
+            ui.label("Display:");
+
+            if ui.button("Load ROM").clicked() {
+                // Load ROM logic
+                emulator.memorymap();
+            }
+        });
+    })
 }
